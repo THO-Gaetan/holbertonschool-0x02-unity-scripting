e82dadfc9fd532e574284e0b557cfda20466733e
Islian <78454382+Islian@users.noreply.github.com>
2026-05-08 17:11:10 +0200

    exercice 1

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(0, 5, -7);
+    public float smoothSpeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+        transform.LookAt(player.position + Vector3.up * 1.5f);
+    }
+}
