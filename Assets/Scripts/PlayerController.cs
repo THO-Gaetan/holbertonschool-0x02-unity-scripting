83e5eb73f6dbc5e428a0c5211f2d9997e8e44c02
Islian <78454382+Islian@users.noreply.github.com>
2026-05-09 12:39:36 +0200

    exercice 7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public int health = 5;
     private int score = 0;
     Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -39,6 +40,21 @@ public class PlayerController : MonoBehaviour
             score += 1;
             Debug.Log("Score: " + score);
         }
+        if (other.gameObject.CompareTag("Trap"))
+        {
+            health -= 1;
+            Debug.Log("Health: " + health);
+        }
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            if (score >= 3)
+            {
+                Debug.Log("You win!");
+            }
+            else
+            {
+                Debug.Log("You need at least 3 points to win!");
+            }
+        }
     }
-
 }
